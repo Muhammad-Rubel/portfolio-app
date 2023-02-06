e36b703249cb99c771e1fb329527311824fba487
Muhammad-Rubel <rubel.contact@gmail.com>
2023-02-06 19:18:04 +0600

    removed path

--- svelte.config.js
+++ svelte.config.js
@@ -14,10 +14,10 @@ const config = {
 			fallback: null,
 			precompress: false,
 			strict: true
-		}),
-		paths: {
-			base: '/portfolio-app'
-		}
+		})
+		// paths: {
+		// 	base: '/portfolio-app'
+		// }
 	}
 };
 
